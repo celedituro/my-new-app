db125facc7bb29f36b21db8cfef31a3c1131e071
celedituro <cdituro@fi.uba.ar>
2023-02-13 12:32:08 -0300

    fix: conflicts with node action

--- ClientApp/src/AppRoutes.js
+++ ClientApp/src/AppRoutes.js
@@ -1,7 +1,6 @@
 import { Counter } from "./components/Counter";
 import { FetchData } from "./components/FetchData";
 import { Home } from "./components/Home";
-import PeopleTable from "./components/PeopleTable";
 
 const AppRoutes = [
   {
