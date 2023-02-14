82957fc76f7a2250f7f3613eabd00feaada3e05e
celedituro <cdituro@fi.uba.ar>
2023-02-14 11:10:02 -0300

    feat: add search bar

--- ClientApp/src/components/FetchData.js
+++ ClientApp/src/components/FetchData.js
@@ -1,58 +1,91 @@
-import React, { Component } from 'react';
+import { useEffect, useState} from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import '../FetchData.css';
 
-export class FetchData extends Component {
-  static displayName = FetchData.name;
+const FetchData = () => {
+    const [search, setSearch] = useState("");
+    const [people, setPeople] = useState([]);
+    const [peopleTable, setPeopleTable] = useState([]);
 
-  constructor(props) {
-    super(props);
-    this.state = { people: [], loading: true };
-  }
+    const getPeople = async () => {
+        try {
+            const response = await axios.get("https://localhost:44425/person");
+            setPeople(response.data);
+            setPeopleTable(response.data);
+            console.log(response.data);
+        } catch(error) {
+            console.log(error);
+        }
+    }
 
-  componentDidMount() {
-    this.populatePeopleData();
-  }
+    useEffect(() => {
+        getPeople();
+    }, [])
 
-  static renderPeopleTable(people) {
+    const handleChange = e => {
+        console.log("Búsqueda:", e.target.value);
+        setSearch(e.target.value);
+        filter(e.target.value);
+    }
+
+    const filter = (query) => {
+        // eslint-disable-next-line array-callback-return
+        var searchResult = peopleTable.filter((element) => {
+            console.log('query:', query);
+            console.log('element:', element);
+            var elementName = element.name.toString().toLowerCase();
+            console.log('elementName:', elementName);
+            var elementDateOfBirth = element.dateOfBirth.toString();
+            console.log('elementDate:', elementDateOfBirth);
+            if(elementName.includes(query.toLowerCase())
+            || elementDateOfBirth.includes(query)){
+                return element;
+            } 
+        });
+        setPeople(searchResult);
+    }
+
     return (
-      <table className="table table-striped" aria-labelledby="tableLabel">
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Name</th>
-            <th>DateOfBirth</th>
-            <th>Category</th>
-          </tr>
-        </thead>
-        <tbody>
-          {people.map(person =>
-            <tr key={person.id}>
-              <td>{person.id}</td>
-              <td>{person.name}</td>
-              <td>{person.dateOfBirth}</td>
-              <td>{person.category}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    );
-  }
-
-  render() {
-    let contents = this.state.loading
-      ? <p><em>Loading...</em></p>
-      : FetchData.renderPeopleTable(this.state.people);
-
-    return (
-      <div>
-        <h1 id="tableLabel">People Table</h1>
-        {contents}
-      </div>
-    );
-  }
-
-  async populatePeopleData() {
-    const response = await fetch('person');
-    const data = await response.json();
-    this.setState({ people: data, loading: false });
-  }
+        <div className="App">
+                <form className="containerSearch">
+                    <input
+                        className="form-control inputSearch"
+                        value={search}
+                        placeholder="Búsqueda por Nombre o Fecha de nacimiento"
+                        onChange={handleChange}
+                    />
+                    <button type="submit" className="btn btn-success buttonSearch">
+                        <FontAwesomeIcon icon={faSearch}/>
+                    </button>
+                </form>
+            <div className="table-responsive">
+                <table className="table table-striped" aria-labelledby="tableLabel">
+                    <thead>
+                        <tr>
+                        <th>Id</th>
+                        <th>Nombre</th>
+                        <th>Fecha de nacimiento</th>
+                        <th>Categoria</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {people.map(person =>
+                        <tr key={person.id}>
+                            <td>{person.id}</td>
+                            <td>{person.name}</td>
+                            <td>{person.dateOfBirth}</td>
+                            <td>{person.category}</td>
+                        </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+      );
 }
+
+export default FetchData;
+
