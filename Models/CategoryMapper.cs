5cfc0e0df56f80af98be5358c2e11fee25866ce1
celedituro <cdituro@fi.uba.ar>
2023-02-17 11:51:40 -0300

    feat: add CategoryMapper

---
+++ Models/CategoryMapper.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_new_app.Models
+{
+    public class CategoryMapper
+    {
+        public const int CHILD_AGE_MAX = 10;
+        public const int TEEN_AGE_MAX = 17;
+        public const int ADULT_AGE_MAX = 79;
+        public const String CHILD = "Niño";
+        public const String TEEN = "Adolescente";
+        public const String ADULT = "Adulto";
+        public const String OCTOGENARIAN = "Octogenario";
+
+        private AgeCalculator calculator;
+        public CategoryMapper(AgeCalculator calculator)
+        {
+            this.calculator = calculator;
+        }
+
+        public String Map(DateTime birth)
+        {
+            int age = this.calculator.Calculate(birth);
+            if(age <= CHILD_AGE_MAX)
+            {
+                return CHILD;
+            } else if(age > CHILD_AGE_MAX && age <= TEEN_AGE_MAX)
+            {
+                return TEEN;
+            } else if(age > TEEN_AGE_MAX && age <= ADULT_AGE_MAX)
+            {
+                return ADULT;
+            } else 
+            {
+                return OCTOGENARIAN;
+            }
+        }
+    }
+}
---
+++ Tests/CategoryMapperTest.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using my_new_app.Models;
+using Xunit;
+
+namespace Tests
+{
+    public class CategoryMapperTest
+    {
+        [Fact]
+        public void ChildTest()
+        {   
+            DateTime birth = new DateTime(2022,02,01);
+            AgeCalculator calculator = new AgeCalculator();
+            CategoryMapper mapper = new CategoryMapper(calculator);
+
+            String name = mapper.Map(birth);
+            String nameExpected = "Niño";
+            
+            Assert.Equal(nameExpected, name);
+        }
+
+        [Fact]
+        public void TeenTest()
+        {   
+            DateTime birth = new DateTime(2006,02,01);
+            AgeCalculator calculator = new AgeCalculator();
+            CategoryMapper mapper = new CategoryMapper(calculator);
+
+            String name = mapper.Map(birth);
+            String nameExpected = "Adolescente";
+
+            Assert.Equal(nameExpected, name);
+        }
+
+        [Fact]
+        public void AdultTest()
+        {   
+            DateTime birth = new DateTime(1974,02,01);
+            AgeCalculator calculator = new AgeCalculator();
+            CategoryMapper mapper = new CategoryMapper(calculator);
+
+            String name = mapper.Map(birth);
+            String nameExpected = "Adulto";
+
+            Assert.Equal(nameExpected, name);
+        }
+
+        [Fact]
+        public void OctogenarianTest()
+        {   
+            DateTime birth = new DateTime(1930,02,01);
+            AgeCalculator calculator = new AgeCalculator();
+            CategoryMapper mapper = new CategoryMapper(calculator);
+
+            String name = mapper.Map(birth);
+            String nameExpected = "Octogenario";
+
+            Assert.Equal(nameExpected, name);
+        }
+    }
+}
